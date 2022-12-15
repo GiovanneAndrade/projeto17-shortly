e77c4b9ca0d85766d53ae5e6c23f187421abf8ba
GiovanneAndrade <vanne.as@hotmail.com>
2022-12-15 20:21:51 -0300

    :sparkles: feat: post  urls repository

--- src/repositories/urls.repository.js
+++ src/repositories/urls.repository.js
@@ -3,11 +3,25 @@ import connection from "../database/db.js";
 async function getMeUrlsRepository() {
   const result = await connection.query(
     `
-      SELECT * FROM users;
+      SELECT * FROM urls;
     `
   );
   return result;
 }
+async function postUrlsRepository({ url, userId, shortUrl, visitCount }) {
+  const result = await connection.query(
+    `
+      INSERT INTO urls (
+        url, 
+        "userId", 
+        "shortUrl", 
+        "visitCount"
+      )
+      VALUES ( $1, $2, $3, $4);
+    `,
+    [url, userId, shortUrl, visitCount]
+  );
+  return result;
+}
 
-export { getMeUrlsRepository };
-   
+export { getMeUrlsRepository, postUrlsRepository };

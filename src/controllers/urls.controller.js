d0a8bdb865b879bd3870ecc3d589852344005c7f
GiovanneAndrade <vanne.as@hotmail.com>
2022-12-15 20:22:29 -0300

    :sparkles: feat: post  urls controller

--- src/controllers/urls.controller.js
+++ src/controllers/urls.controller.js
@@ -1,5 +1,5 @@
 import * as allUrls from "../repositories/urls.repository.js";
-
+import { nanoid } from 'nanoid'
 async function getMeUrlsController(req, res) {
   try {    
     const result = await allUrls.getMeUrlsRepository();
@@ -8,5 +8,18 @@ async function getMeUrlsController(req, res) {
     return res.sendStatus(500).send(error);
   }
 }
+    
+async function postUrlsController(req, res) {
+  const { url } = req.body
+  const userId = 1
+  const shortUrl = nanoid()
+  const visitCount = 0
+  try {    
+    await allUrls.postUrlsRepository({ url, userId, shortUrl, visitCount });
+    return res.status(201).send({shortUrl});
+  } catch (error) {
+    return res.sendStatus(500).send(error);
+  }
+}
 
-export { getMeUrlsController }
+export { getMeUrlsController, postUrlsController }

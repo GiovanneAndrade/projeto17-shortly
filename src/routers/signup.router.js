1ed4a19bfa0fee869284b3aeef0e4082d7895363
GiovanneAndrade <vanne.as@hotmail.com>
2022-12-15 20:21:29 -0300

    :sparkles: feat: post  signup router

---
+++ src/routers/signup.router.js
@@ -0,0 +1,12 @@
+import { Router } from "express"
+import { postSignupController } from "../controllers/signup.controller.js"
+ 
+ 
+   
+
+const router = Router() 
+  
+router.post('/signup', postSignupController)
+ 
+
+export default router

dccfd5115897155ae4625e092f3645bf46fe7875
GiovanneAndrade <vanne.as@hotmail.com>
2022-12-16 17:33:34 -0300

    :sparkles: feat: get consult  signup repository

--- src/repositories/signup.repository.js
+++ src/repositories/signup.repository.js
@@ -1,18 +1,27 @@
 import connection from "../database/db.js";
 
+
+async function getConsultSignupRepository({email}) {
+  const result = await connection.query(
+    `
+      SELECT * FROM users WHERE email = '${email}'
+   `
+  );
+  return result;
+}
 async function postSignupRepository({ name, email, password }) {
   const result = await connection.query(
     `
-    INSERT INTO users ( 
-      name, 
-      email,
-      password
-    ) 
-    VALUES 
-      ($1, $2, $3)
+      INSERT INTO users ( 
+        name, 
+        email,
+        password
+      ) 
+      VALUES 
+        ($1, $2, $3)
    `,
     [name, email, password]
   );
   return result;
 }
-export { postSignupRepository };
+export { postSignupRepository, getConsultSignupRepository };

401f63fcd97461d6912e9aaf14ce9c279f7349ef
GiovanneAndrade <vanne.as@hotmail.com>
2022-12-19 18:30:06 -0300

    :sparkles: feat: post  url  middlewares

--- src/middlewares/urls.middlewares.js
+++ src/middlewares/urls.middlewares.js
@@ -1,7 +1,14 @@
- 
+import * as allUrls from "../repositories/urls.repository.js";
 import * as allSessions from "../repositories/sessions.repository.js";
 import { postUrlSchema } from "../schemas/urls.schema.js";
-
+async function postUrlsMiddlewares(req, res, next) {
+  const idUrl = req.params.id;
+  const result = await allUrls.getIdUrlsRepository({ idUrl });
+  if (result.rows.length === 0) {
+    return res.sendStatus(404);
+  }
+  next();
+}
 
 async function postUrlShortenMiddlewares(req, res, next) {
   const { authorization } = req.headers; 
@@ -26,4 +33,4 @@ async function postUrlShortenMiddlewares(req, res, next) {
 
   next(); 
 }
-export {   postUrlShortenMiddlewares };
+export { postUrlsMiddlewares, postUrlShortenMiddlewares };

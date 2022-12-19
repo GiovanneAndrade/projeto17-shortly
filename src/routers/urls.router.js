5b7fcc4b618cf833ce3a732021d411a39a22087b
GiovanneAndrade <vanne.as@hotmail.com>
2022-12-19 18:23:51 -0300

    :sparkles: feat: get  urls router

--- src/routers/urls.router.js
+++ src/routers/urls.router.js
@@ -1,11 +1,11 @@
 import { Router } from "express"
-import {   postUrlsController } from "../controllers/urls.controller.js"
-import { postUrlShortenMiddlewares } from "../middlewares/urls.middlewares.js"
+import { getIdUrlsController, postUrlsController } from "../controllers/urls.controller.js"
+import { postUrlShortenMiddlewares, postUrlsMiddlewares } from "../middlewares/urls.middlewares.js"
    
 
 const router = Router() 
     
-
+router.get('/urls/:id', postUrlsMiddlewares, getIdUrlsController)
 router.post('/urls/shorten', postUrlShortenMiddlewares, postUrlsController)
  
 

f9ceed44ad70f9aa8d3429211d5e70315400afdd
GiovanneAndrade <vanne.as@hotmail.com>
2022-12-19 18:23:18 -0300

    :sparkles: feat: post  urls router

--- src/routers/urls.router.js
+++ src/routers/urls.router.js
@@ -1,11 +1,12 @@
 import { Router } from "express"
-import { getMeUrlsController, postUrlsController } from "../controllers/urls.controller.js"
+import {   postUrlsController } from "../controllers/urls.controller.js"
+import { postUrlShortenMiddlewares } from "../middlewares/urls.middlewares.js"
    
 
 const router = Router() 
     
-router.get('/urls', getMeUrlsController)
-router.post('/urls', postUrlsController)
+
+router.post('/urls/shorten', postUrlShortenMiddlewares, postUrlsController)
  
 
 export default router

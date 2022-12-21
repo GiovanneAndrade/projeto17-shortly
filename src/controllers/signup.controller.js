95b35f9b839d4c87656c0ca06d8d9fbccf273973
GiovanneAndrade <vanne.as@hotmail.com>
2022-12-21 17:56:30 -0300

    :bug: fix:  to remove email

--- src/controllers/signup.controller.js
+++ src/controllers/signup.controller.js
@@ -5,15 +5,6 @@ async function postSignupController(req, res) {
   let { name, email, password } = req.body;
 
   name = name.trim().replace(/( )+/g, " ");
-  let novoEmail = email.replace(/[^a-z0-9]/gi, "");
-  let novoPassword = password.replace(/[^a-z0-9]/gi, "");
-  
-  if (
-    email !== novoEmail ||
-    password !== novoPassword
-  ) {
-    return res.sendStatus(404);
-  }
   password = bcrypt.hashSync(req.body.password, 10);
 
   try {

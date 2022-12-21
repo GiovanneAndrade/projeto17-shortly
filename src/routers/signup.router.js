998dec1b8aa7f54425ffa70123aa75a32e747df4
GiovanneAndrade <vanne.as@hotmail.com>
2022-12-21 18:29:34 -0300

    :ok_hand: style: settings

--- src/routers/signup.router.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { postSignupController } from "../controllers/signup.controller.js";
-import { postSignupMiddlewares } from "../middlewares/signup.middlewares.js";
-
-const router = Router();
-
-router.post("/signup", postSignupMiddlewares, postSignupController);
-
-export default router;

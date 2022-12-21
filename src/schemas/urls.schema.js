c19a7099d1ebcbf622b533312591135677990a6c
GiovanneAndrade <vanne.as@hotmail.com>
2022-12-21 18:28:26 -0300

    :sparkles: feat:  get urls schema

--- src/schemas/urls.schema.js
+++ src/schemas/urls.schema.js
@@ -5,5 +5,7 @@ import Joi from 'joi';
 const postUrlSchema = Joi.object({
   url : Joi.string().uri().required().min(1)
 })
-
-export { postUrlSchema }
+const getUrlsOpenSchema = Joi.object({
+  shortUrl : Joi.string().required().min(1).trim()
+})
+export { postUrlSchema, getUrlsOpenSchema }
